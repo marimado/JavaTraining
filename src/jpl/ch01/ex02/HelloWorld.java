edc723715d014ec22e96ec7615254bdb4a9c07dc
mariko.madono <mariko.madono@ZRFG029418.rfg.rfgad.ricoh.com>
2016-09-29 14:58:43 +0900

    added

---
+++ src/jpl/ch01/ex02/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch01.ex02;
+
+/**
+ *
+ * @author mariko.madono
+ */
+public class HelloWorld {
+    
+}
