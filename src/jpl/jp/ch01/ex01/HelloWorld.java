00ee592321a2ed2eda6aa9712d2bba21a96b7f63
mariko.madono <mariko.madono@ZRFG029418.rfg.rfgad.ricoh.com>
2016-10-13 10:34:55 +0900

    modified

--- src/jpl/jp/ch01/ex01/HelloWorld.java
+++ src/jpl/jp/ch01/ex01/HelloWorld.java
@@ -1,7 +1,7 @@
 // Copyright (C) 2016
 package jp.ch01.ex01;
 
-class HelloWorld {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
---
+++ src/jpl/jp/ch01/ex03/FibonacciSequence.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.ch01.ex03;
+/**
+ *
+ * @author mariko.madono
+ */
+public class FibonacciSequence {
+   //値が50未満のフィボナッチ数列を表示します。
+    public static void main(String[] args){
+        System.out.println("＊＊値が50未満のフィボナッチ数列の数字リスト＊＊");
+        //フィボナッチ数列の前数項がlo,フィボナッチ数列の現在の項は、hi
+        int lo =1;
+        int hi =1;
+        System.out.println(lo);
+        while(hi < 50 ){
+            System.out.println(hi);
+            hi = hi + lo;
+            lo = hi - lo;
+        }
+    }
+}
