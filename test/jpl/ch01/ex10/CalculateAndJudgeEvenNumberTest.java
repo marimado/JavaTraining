e6f76d90f3854b8114f927486679a9907c6c44b9
mariko.madono <mariko.madono@ZRFG029418.rfg.rfgad.ricoh.com>
2016-10-26 16:53:02 +0900

    added jpl.chp1.ex10,ex11, and ex12

---
+++ test/jpl/ch01/ex10/CalculateAndJudgeEvenNumberTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2016 Mariko Madono. All rights reserved.
+ */
+package jpl.ch01.ex10;
+
+import ch01.ex10.CalculateAndJudgeEvenNumber;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+
+/**
+ *
+ * @author mariko.madono
+ */
+public class CalculateAndJudgeEvenNumberTest {
+    @Test
+    public void testMain(){
+    //フィボナッチ数列をつくる。
+    //期待する配列の値、1,1,2,3,5,8,13,21,34を検証する。
+    //期待する配列の値、false,false,true,false,false,true,false,false,trueを検証する。
+    //Prepare
+        int MAX_INDEX = 9;
+        int hi = 1;
+        int lo = 1;
+        int[] intExpected = new int[]{1,1,2,3,5,8,13,21,34};
+        //int[] actual = new int[MAX_INDEX];
+        CalculateAndJudgeEvenNumber cal = new CalculateAndJudgeEvenNumber();
+        boolean[] boolExpected = new boolean[]{false,false,true,false,false,true,false,false,true};
+        
+    //Action
+    int[] intActual = cal.storeData(lo, hi, MAX_INDEX);
+    boolean[] boolActual = cal.judgeEven(intActual, MAX_INDEX);
+    
+//Check
+    assertThat(intActual, is(intExpected));
+    assertThat(boolActual, is(boolExpected));
+    } 
+}
